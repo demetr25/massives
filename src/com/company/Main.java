3d09dd423ad664ecfcc45e73f8ae9f02e92342ce
Admin <dmitrijdundakov@gmail.com>
2021-10-13 21:12:32 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int[] arr = {9,2,4,5,6,7,9};
+        for (int i = 0; i < arr.length; i++) {
+            int value = arr[i];
+            System.out.println(i + " " + value);
+        }
+    }
+}
